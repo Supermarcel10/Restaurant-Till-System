ca4a74e9532c1684662aaf59a34499ab692e614e
Marcel <marcelbarlik@gmail.com>
2021-03-20 00:57:51 +0000

    v1.0

--- config/declarations.py
+++ config/declarations.py
@@ -1,33 +1,39 @@
-def average(*args):
-    print(args)
-    return args
+from pathlib import Path
+from typing import Dict
 
 
 def declarations():
-    pizza_types = {"Cheese and Tomato": 3.5,
-                   "Ham and pineapple": 4.2,
-                   "Vegetarian": 5.2,
-                   "Meat feast": 5.8,
-                   "Seafood": 5.6
-                   }
+    pizza_types: Dict[str, float] = {"Cheese and Tomato": 3.5,
+                                     "Ham and pineapple": 4.2,
+                                     "Vegetarian": 5.2,
+                                     "Meat feast": 5.8,
+                                     "Seafood": 5.6
+                                     }
 
-    pizza_bases = {"Traditional": 1,
-                   "Thin and Crispy": 1
-                   }
+    pizza_bases: Dict[str, float] = {"Traditional": 1,
+                                     "Thin and Crispy": 1
+                                     }
 
-    pizza_toppings = {"Cheese": 0.5,
-                      "Pepperoni": 0.5,
-                      "Onions": 0.5,
-                      "Peppers": 0.5
-                      }
+    pizza_toppings: Dict[str, float] = {"Cheese": 0.5,
+                                        "Pepperoni": 0.5,
+                                        "Onions": 0.5,
+                                        "Peppers": 0.5
+                                        }
 
-    drink = {"Cola": 0.9,
-             "Lemonade": 0.8,
-             "Fizzy orange": 0.9
-             }
+    drink: Dict[str, float] = {"Cola": 0.9,
+                               "Lemonade": 0.8,
+                               "Fizzy orange": 0.9
+                               }
+
 
     configs = {}
-    with open("config/UserDefinedConfig.config", "r+") as config_file: #Todo: Fix
+    config_path = Path("config/UserDefinedConfig.config")
+
+    if not config_path.exists():
+        print("creating file")
+        open(config_path, "a")
+
+    with open(config_path, "r+") as config_file:
         lines_config_file = config_file.readlines()
         i = len(lines_config_file)
         while 0 < i:
